de20c8240949192abe9b67ac2c327dd8ec6ef76e
Abhishek Singh <singh4758@gmail.com>
2023-05-22 14:40:03 +0530

    feat: About design pattern

---
+++ 2. creation design pattern/1. design pattern.ts
@@ -0,0 +1,19 @@
+/*
+  What is design Pattern ?
+  Generalised, reusable solutions to common design issue in software engineering.
+
+  Creational:-  
+  creational patterns are all about instantiating objects how
+  objects are instantiated and how many of them we are allowed to create.
+  They offer this kind of control on the creation of objects and their responsibility stops there.
+
+  Structural:-
+  Then we have structural design patterns that are about you guessed it the app's structure.
+  Sometimes you have complex classes or objects that are composed from smaller classes or objects.
+  Structural design patterns are for the most efficient way to work in situations like this and solve
+  any challenges that may arise.
+
+  Behavioural:-
+  Finally behavioral patterns determine ways of application flow and how objects communicate with each
+  other.
+*/
