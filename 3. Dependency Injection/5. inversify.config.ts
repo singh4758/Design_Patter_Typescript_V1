584436a8cf32a343fad8e0091df07d69798d4528
Abhishek Singh <singh4758@gmail.com>
2023-06-02 20:52:25 +0530

    type of scope in inversify

--- 3. Dependency Injection/5. inversify.config.ts
+++ 3. Dependency Injection/5. inversify.config.ts
@@ -9,9 +9,15 @@ import { IDepB } from "./5. idepB";
 import { IDepC } from "./5. idepC";
 import { ConcreteC } from "./5. concreteC";
 
-let container = new Container();
-container.bind<IDepA>(TYPES.IDepA).to(ConcreteA);
-container.bind<IDepB>(TYPES.IDepA).to(ConcreteB);
-container.bind<IDepC>(TYPES.IDepA).to(ConcreteC);
+let container = new Container({
+  defaultScope: 'Singleton',
+});
+
+// IDepA will be singleton
+container.bind<IDepA>(TYPES.IDepA).to(ConcreteA).inSingletonScope();
+//IDepB will be transient. We will get a new instance every time we request one
+container.bind<IDepB>(TYPES.IDepA).to(ConcreteB).inTransientScope();
+// IDepC  will be request scope. It will return same instance until we call container.unbind
+container.bind<IDepC>(TYPES.IDepA).to(ConcreteC).inRequestScope();
 
 export default container;
